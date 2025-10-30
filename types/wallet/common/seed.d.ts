7f4e66b6c410fd827c66dbaeeb2ae12b635debe9
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-10-30 12:57:38 +0400

    Run tsc

--- types/wallet/common/seed.d.ts
+++ types/wallet/common/seed.d.ts
@@ -1,6 +1,6 @@
+export type Descriptor = any;
 export class Seed {
     /**
-     *
      * @param {string|Uint8Array|Buffer|number[]} input
      * @returns {Seed}
      */
@@ -8,8 +8,32 @@ export class Seed {
     /** @param {Uint8Array|number[]} bytes length 48 */
     constructor(bytes: Uint8Array | number[]);
     bytes: Uint8Array;
+    /** @returns {Uint8Array} */
     hashSHA256(): Uint8Array;
     /** @returns {Uint8Array} */
     toBytes(): Uint8Array;
+}
+export class ExtendedSeed {
+    /**
+     *
+     * @param {Descriptor} desc
+     * @param {Seed} seed
+     * @returns {ExtendedSeed}
+     */
+    static newExtendedSeed(desc: any, seed: Seed): ExtendedSeed;
+    /**
+     *
+     * @param {string|Uint8Array|Buffer|number[]} input
+     * @returns {ExtendedSeed}
+     */
+    static from(input: string | Uint8Array | Buffer | number[]): ExtendedSeed;
+    constructor(bytes: any);
+    bytes: Uint8Array;
+    /** @returns {Uint8Array} */
+    getDescriptorBytes(): Uint8Array;
+    /** @returns {Uint8Array} */
+    getSeedBytes(): Uint8Array;
+    /** @returns {Seed} */
+    getSeed(): Seed;
 }
 //# sourceMappingURL=seed.d.ts.map
--- types/wallet/ml_dsa_87/wallet.d.ts
+++ types/wallet/ml_dsa_87/wallet.d.ts
@@ -1,8 +1,10 @@
+export type Descriptor = import('../common/descriptor.js').Descriptor;
 export class Wallet {
     static newWallet(metadata?: number[]): Wallet;
     static newWalletFromSeed(seed: any, metadata?: number[]): Wallet;
     static newWalletFromExtendedSeed(extendedSeed: any): Wallet;
     static newWalletFromMnemonic(mnemonic: any): Wallet;
+    static verify(signature: any, message: any, pk: any): boolean;
     constructor({ descriptor, seed, pk, sk }: {
         descriptor: any;
         seed: any;
@@ -13,15 +15,25 @@ export class Wallet {
     seed: any;
     pk: any;
     sk: any;
+    extendedSeed: ExtendedSeed;
     /** @returns {Uint8Array} length 20 */
     getAddress(): Uint8Array;
     /** @returns {string} "Q" + hex */
     getAddressStr(): string;
-    getDescriptor(): any;
+    /** @returns {Descriptor} */
+    getDescriptor(): Descriptor;
+    /** @returns {ExtendedSeed} */
+    getExtendedSeed(): ExtendedSeed;
+    /** @returns {Seed} */
+    getSeed(): Seed;
     /** @returns {string} hex(Seed) */
     getHexSeed(): string;
     /** @returns {Uint8Array} */
     getPK(): Uint8Array;
+    /** @returns {Uint8Array} */
+    getSK(): Uint8Array;
     sign(message: any): Uint8Array;
 }
+import { ExtendedSeed } from "../common/seed.js";
+import { Seed } from "../common/seed.js";
 //# sourceMappingURL=wallet.d.ts.map
