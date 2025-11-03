7354d74343b4b44f33facb7e8e1c37fdff089d2f
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-11-03 23:59:53 +0400

    Remove sphincs files

--- types/wallet/sphincsplus_256s/wallet.d.ts
+++
@@ -1,9 +0,0 @@
-export class Wallet {
-    /** @returns {Uint8Array} */ getAddress(): Uint8Array;
-    /** @returns {string} */ getAddressStr(): string;
-    /** @returns {Uint8Array} */ getPK(): Uint8Array;
-    /** @returns {Uint8Array} */ getSK(): Uint8Array;
-    /** @param {Uint8Array} message @returns {Uint8Array} */ sign(): Uint8Array;
-    /** @param {ExtendedSeed} @returns {Uint8Array} */ newWalletFromHexSeed(): Uint8Array;
-}
-//# sourceMappingURL=wallet.d.ts.map
