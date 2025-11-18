25fcd68b1bb7b53097ce6eb3bb85f6d34011301c
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-11-18 14:44:49 +0400

    Review files

--- src/index.js
+++ src/index.js
@@ -8,22 +8,18 @@ const { getAddressFromPKAndDescriptor } = require('./wallet/common/address.js');
 const { ExtendedSeed, Seed } = require('./wallet/common/seed.js');
 const { newMLDSA87Descriptor } = require('./wallet/ml_dsa_87/descriptor.js');
 const { Descriptor } = require('./wallet/common/descriptor.js');
-const { newWalletFromExtendedSeed } = require('./wallet/factory.js');
+const { newWalletFromExtendedSeed: fromExtendedSeed } = require('./wallet/factory.js');
 const { Wallet: MLDSA87 } = require('./wallet/ml_dsa_87/wallet.js');
 const { WalletType } = require('./wallet/common/wallettype.js');
 
 module.exports = {
-  Seed,
-  SEED_SIZE,
-  ExtendedSeed,
-  EXTENDED_SEED_SIZE,
-  Descriptor,
-  DESCRIPTOR_SIZE,
-  newMLDSA87Descriptor,
+  Seed, SEED_SIZE,
+  ExtendedSeed, EXTENDED_SEED_SIZE,
+  Descriptor, DESCRIPTOR_SIZE, newMLDSA87Descriptor,
   getAddressFromPKAndDescriptor,
   WalletType,
   Wallet: {
-    newWalletFromExtendedSeed,
+    fromExtendedSeed,
     MLDSA87,
   },
 };
